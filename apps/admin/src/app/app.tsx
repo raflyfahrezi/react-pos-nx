11f9542697f840b4c537e0fbfd2271978a0016a4
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2023-02-08 09:32:04 +0700

    init admin react apps

---
+++ apps/admin/src/app/app.tsx
@@ -0,0 +1,35 @@
+import './app.css';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="admin" />
+      <div />
+    </>
+  );
+}
+
+export default App;
+
+if (import.meta.vitest) {
+  // add tests related to your file here
+  // For more information please visit the Vitest docs site here: https://vitest.dev/guide/in-source.html
+
+  const { it, expect, beforeEach } = import.meta.vitest;
+  let render: any;
+
+  beforeEach(async () => {
+    render = (await import('@testing-library/react')).render;
+  });
+
+  it('should render successfully', () => {
+    const { baseElement } = render(<App />);
+    expect(baseElement).toBeTruthy();
+  });
+
+  it('should have a greeting as the title', () => {
+    const { getByText } = render(<App />);
+    expect(getByText(/Welcome admin/gi)).toBeTruthy();
+  });
+}
